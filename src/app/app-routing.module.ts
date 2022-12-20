83a1c571c3845fe15abc193151780e66ad781327
subbareddykovvuri <subbareddy934@gmail.com>
2022-12-21 01:53:14 +0530

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChartsComponent } from './routes/charts/charts.component';
+import { LoginComponent } from './routes/login/login.component';
+import { PasswordComponent } from './routes/password/password.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { ToggleComponent } from './toggle/toggle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:NavbarComponent},
+  {path:'static',component:ToggleComponent,pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'changepassword',component:PasswordComponent},
+  {path:'charts',component:ChartsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { RoutesComponent } from './routes/routes.component';
+import { ChartsComponent } from './routes/charts/charts.component';
+import { LoginComponent } from './routes/login/login.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { PasswordComponent } from './routes/password/password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ToggleComponent,
+    RoutesComponent,
+    ChartsComponent,
+    LoginComponent,
+    RegisterComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
